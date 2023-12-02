6c136858a39426969a4fd781808f492c3c528b00
Silmar <silmarjr2@gmail.com>
2023-12-02 00:18:31 -0300

    update main to process arguments

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,17 @@
 use core::num::ParseIntError;
 
 fn main() {
-   let _a = get_calibration_value("1abc2");
-   let _b = get_calibration_value("pqr3stu8vwx");
-   let _c = get_calibration_value("a1b2c3d4e5f");
-   let _d = get_calibration_value("treb7uchet");
+    let input = std::env::args().nth(1).unwrap();
+
+    let mut acumulator = 0;
+   
+    for line in input.lines() {
+        match get_calibration_value(line) {
+            Ok(v) => acumulator += v,
+            Err(_) => {}
+        }
+    }
+   println!("{}", acumulator);
 }
 
 fn get_calibration_value(line: &str) -> Result<i32, ParseIntError> {
